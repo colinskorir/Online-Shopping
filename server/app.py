1ddb768f98029a4dffe810862b02504f008f9b1a
collinskorir <korircoll20@gmail.com>
2025-06-26 17:28:06 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -4,7 +4,6 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 from datetime import datetime
-from twilio.rest import Client
 import requests
 import base64
 import time
